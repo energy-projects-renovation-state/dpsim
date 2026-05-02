ba15a5a6ac2927236bd4d38504f721367db822e2
Georgii Tishenin <georgii.tishenin@eonerc.rwth-aachen.de>
2026-05-02 13:35:30 +0200

    Refactor EMT 3-phase SSN inductor and serial RLC to reuse base classes
    Signed-off-by: Georgii Tishenin <georgii.tishenin@eonerc.rwth-aachen.de>

--- dpsim-models/include/dpsim-models/EMT/EMT_Ph3_SSN_Inductor.h
+++ dpsim-models/include/dpsim-models/EMT/EMT_Ph3_SSN_Inductor.h
@@ -8,8 +8,7 @@
 #pragma once
 
 #include <dpsim-models/Base/Base_Ph3_Inductor.h>
-#include <dpsim-models/MNASimPowerComp.h>
-#include <dpsim-models/Solver/MNAInterface.h>
+#include <dpsim-models/EMT/EMT_Ph3_TwoTerminalVTypeSSNComp.h>
 
 namespace CPS {
 namespace EMT {
@@ -17,20 +16,19 @@ namespace Ph3 {
 namespace SSN {
 /// \brief Inductor
 ///
-/// The SSN-inductor is represented by a DC equivalent circuit which
-/// corresponds to one iteration of the trapezoidal integration method.
-/// The equivalent DC circuit is a resistance in parallel with a current
-/// source. The resistance is constant for a defined time step and system
-/// frequency and the current source changes for each iteration.
-///	This means that for the inductor the SSN and Resistive Companion models
-/// are conceptionally the same and only variable names, structures and
-/// state update timings differ. This component is meant to show a V-type SSN
-/// model implementation based on a simple example element. Due to additional
-/// calculation steps, the RC model should be used over this otherwise.
-class Inductor final : public MNASimPowerComp<Real>,
+/// Three-phase SSN inductor implemented as a specialized two-terminal,
+/// V-type SSN component.
+///
+/// State-space choice:
+///   x = i_abc
+///   u = v_abc
+///   y = i_abc
+class Inductor final : public TwoTerminalVTypeSSNComp,
                        public Base::Ph3::Inductor,
                        public SharedFactory<Inductor> {
 public:
+  using SharedFactory<Inductor>::make;
+
   /// Defines UID, name, component parameters and logging level
   Inductor(String uid, String name,
            Logger::Level logLevel = Logger::Level::off);
@@ -40,45 +38,7 @@ public:
 
   SimPowerComp<Real>::Ptr clone(String name) override;
 
-  // #### General ####
-  /// Initializes component from power flow data
-  void initializeFromNodesAndTerminals(Real frequency) override;
-
-  // #### MNA section ####
-  /// Initializes internal variables of the component
-  void mnaCompInitialize(Real omega, Real timeStep,
-                         Attribute<Matrix>::Ptr leftVector) override;
-  /// Stamps system matrix
-  void mnaCompApplySystemMatrixStamp(SparseMatrixRow &systemMatrix) override;
-  /// Stamps right side (source) vector
-  void mnaCompApplyRightSideVectorStamp(Matrix &rightVector) override;
-  /// Update interface voltage from MNA system result
-  void mnaCompUpdateVoltage(const Matrix &leftVector) override;
-  /// Update interface current from MNA system result
-  void mnaCompUpdateCurrent(const Matrix &leftVector) override;
-  /// MNA pre step operations
-  void mnaCompPreStep(Real time, Int timeStepCount) override;
-  /// MNA post step operations
-  void mnaCompPostStep(Real time, Int timeStepCount,
-                       Attribute<Matrix>::Ptr &leftVector) override;
-  /// Add MNA pre step dependencies
-  void mnaCompAddPreStepDependencies(
-      AttributeBase::List &prevStepDependencies,
-      AttributeBase::List &attributeDependencies,
-      AttributeBase::List &modifiedAttributes) override;
-  /// Add MNA post step dependencies
-  void
-  mnaCompAddPostStepDependencies(AttributeBase::List &prevStepDependencies,
-                                 AttributeBase::List &attributeDependencies,
-                                 AttributeBase::List &modifiedAttributes,
-                                 Attribute<Matrix>::Ptr &leftVector) override;
-
-private:
-  //rightsideVector history term
-  Matrix mHistoricCurrent = Matrix::Zero(3, 1);
-  //dependency on latest Voltage, represented by Conductance in system matrix
-  Matrix mDufourBKHat = Matrix::Zero(3, 3);
-  Matrix mDufourWKN = Matrix::Zero(3, 3);
+  void setParameters(const Matrix &inductance);
 };
 } // namespace SSN
 } // namespace Ph3
